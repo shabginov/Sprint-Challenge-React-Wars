226d38f307f96ed317c04366ed149cb73e25d356
Alexey Shabginov <a.shabginov@gmail.com>
2020-05-29 18:37:45 -0700

    Imports useState, useEffect, axios, retrives and initializes data from an API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import Cards from './Cards';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,22 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([]);
 
+  useState(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      console.log(response.data.results)
+      setData(response.data.results);
+    })
+    .catch(error => console.log(error));
+
+  }, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Cards data={data}/>
     </div>
   );
 }
